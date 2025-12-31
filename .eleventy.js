6a22b18952d3dcc9c64f3630781c2fa5230fef90
Jess Rappaport <jess@goldenchaos.net>
2025-12-31 18:01:55 -0500

    refactor styles to use scss instead of css

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,41 @@
+const sass = require('sass');
+const fs = require('fs');
+
+// Compile SCSS before module exports
+function compileSass() {
+  try {
+    const result = sass.renderSync({
+      file: './style.scss',
+      outputStyle: "expanded"
+    });
+    const css = result.css.toString();
+    
+    // Write to root
+    fs.writeFileSync('./style.css', css, { encoding: 'utf-8' });
+    
+    // Write to _site only if it exists
+    if (fs.existsSync('./_site')) {
+      fs.writeFileSync('./_site/style.css', css, { encoding: 'utf-8' });
+    }
+    console.log('[11ty] SCSS compiled to style.css');
+  } catch(err) {
+    console.error('SCSS compilation error:', err);
+  }
+}
+
+// Initial compilation
+compileSass();
+
 module.exports = function(eleventyConfig) {
+  // Watch SCSS files for changes and recompile
+  eleventyConfig.addWatchTarget("*.scss");
+  eleventyConfig.on("eleventy.before", () => {
+    compileSass();
+  });
+
   // Static assets passthrough
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("fonts");
-  eleventyConfig.addPassthroughCopy("*.css");
-  eleventyConfig.addPassthroughCopy("*.scss");
   eleventyConfig.addPassthroughCopy("og-image.png");
   eleventyConfig.addPassthroughCopy("CNAME");
   eleventyConfig.addPassthroughCopy({
