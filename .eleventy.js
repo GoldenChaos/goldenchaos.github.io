2f84ec1aed30d1e0a1954b0092a41a3a167cb5ad
Jess Rappaport <jess@goldenchaos.net>
2026-01-03 02:06:37 -0500

    fix apple icon again again

--- .eleventy.js
+++ .eleventy.js
@@ -44,6 +44,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("fonts");
   eleventyConfig.addPassthroughCopy("og-image.png");
+  eleventyConfig.addPassthroughCopy("apple-touch-icon-precomposed.png");
   eleventyConfig.addPassthroughCopy("CNAME");
   eleventyConfig.addPassthroughCopy({ "src/manifest.webmanifest": "manifest.webmanifest" });
   eleventyConfig.addPassthroughCopy({ "src/OneSignalSDKWorker.js": "OneSignalSDKWorker.js" });
